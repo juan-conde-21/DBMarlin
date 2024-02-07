8a805d57e77cedd3c83de556ef4dd489238405bb
Juan Conde <juan.conde@ibm.com>
2024-02-07 21:56:57 +0000

    first commit

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS contactos;
+
+USE contactos;
+
+CREATE TABLE IF NOT EXISTS emails(id int AUTO_INCREMENT, firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50), PRIMARY KEY(id));
